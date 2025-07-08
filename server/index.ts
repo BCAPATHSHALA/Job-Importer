2d268107a5561a941c2155f6f86dc92f3143e736
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2025-07-08 13:37:10 +0530

    import both files:worker and cron job in index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
+import "./config/cronJob";
+import "./workers/job.worker";
 
 dotenv.config({ path: ".env" });
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON data from req.body
+app.use(express.json({ limit: "50kb" }));
 
 app.get("/", (_, res) => {
   res.send("Job Importer Running");
 });
 
-const PORT = process.env.PORT || 5000;
-
+// Connect to MongoDB and start the server
 connectDB()
   .then(() => {
     // Start our Express server
